ce311bef4e7dc14895c8dc3a40a02fdb4c26173b
zmarkiss <140748492+zmarkiss@users.noreply.github.com>
2023-09-26 20:19:05 -0700

    Add files via upload

---
+++ Counting unique.py
@@ -0,0 +1,10 @@
+#  Counting unique
+#  unduplicated count of items among multiple list
+
+
+Belov = ["Physics", "Math", "Russian"]
+Smith = ["Math", "Geometry", "English"]
+Sarada = ["Japanese", "Math", "Physics"]
+
+sum_set = len(set(Belov + Sarada + Smith))
+print(sum_set)
