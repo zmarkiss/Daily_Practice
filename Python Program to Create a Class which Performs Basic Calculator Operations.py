aabc36598b22dffa4844375a00581c8b0df3a164
zmarkiss <140748492+zmarkiss@users.noreply.github.com>
2023-09-30 12:47:14 -0700

    Update Python Program to Create a Class which Performs Basic Calculator Operations.py
    Updated code to use class attributes where possible.

--- Python Program to Create a Class which Performs Basic Calculator Operations.py
+++ Python Program to Create a Class which Performs Basic Calculator Operations.py
@@ -1,52 +1,87 @@
 # Python Program to Create a Class which Performs Basic Calculator Operations
-class Operation:
-    def __init__(self, x, oper, y):
+class Hcalc:
+    memory = 0.0
+    counter = 1
+    msg_0 = 'Enter an equation'
+    msg_1 = 'Do you even know what numbers are? Stay focused!'
+    msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+    msg_3 = 'Yeah... division by zero. Smart move...'
+    msg_4 = 'Do you want to store the result? (y / n):'
+    msg_5 = 'Do you want to continue calculations? (y / n):'
+    operations = {"+": lambda x, y: x + y,
+                  "-": lambda x, y: x - y,
+                  "*": lambda x, y: x * y,
+                  "/": lambda x, y: x / y
+                  }
+
+    def __init__(self, x, operator, y):
         self.x = x
-        self.oper = oper
+        self.operator = operator
         self.y = y
-        self.msg_0 = 'Enter an equation'
-        self.msg_1 = 'Do you even know what numbers are? Stay focused!'
-        self.msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
-        self.msg_3 = 'Yeah... division by zero. Smart move...'
 
     def x_y_check(self):
-        if self.x.replace(".", "").isnumeric() is not True or self.y.replace(".", "").isnumeric() is not True:
+        if Hcalc.counter == 1:
+            if self.operator == "/" and (self.y == 0 or self.y.isalpha()):
+                print(Hcalc.msg_3)
+                return True
+            elif self.x.replace(".", "").isnumeric() is not True or self.y.replace(".", "").isnumeric() is not True:
+                print(f'{Hcalc.msg_1}')
+                return True
+            else:
+                Hcalc.counter += 1
+                return False
+        if Hcalc.counter > 1:
+            if self.x.replace(".", "").isnumeric() is not True:
+                self.x = self.memory
+            if self.y.replace(".", "").isnumeric() is not True:
+                self.y = self.memory
+
+    def operator_check(self):
+        if self.operator not in ('+', '-', '/', '*'):
             return True
         else:
             return False
 
-    def oper_check(self):
-        if self.oper not in ('+', '-', '/', '*'):
-            return True
-        else:
-            return False
+    def store_continue(self):
+        store = input(Hcalc.msg_4)
+        if store == 'y':
+            Hcalc.memory = Hcalc.operations[self.operator](self.x, self.y)
+            cont = input(Hcalc.msg_5)
+            if cont == 'y':
+                return True
+            else:
+                return False
+        if store == 'n':
+            cont = input(Hcalc.msg_5)
+            if cont == 'y':
+                return True
+            else:
+                return False
 
     def control_func(self):
-        if Operation.x_y_check(self) is True:
-            print(f'{self.msg_1}')
+        if Hcalc.x_y_check(self) is True:
             return True
-        elif Operation.oper_check(self) is True:
-            print(self.msg_2)
+
+        if Hcalc.operator_check(self) is True:
+            print(Hcalc.msg_2)
             return True
-        elif self.oper == '+':
-            print(float(self.x) + float(self.y))
-            return False
-        elif self.oper == '-':
-            print(float(self.x) - float(self.y))
-            return False
-        elif self.oper == '*':
-            print(float(self.x) * float(self.y))
-            return False
-        elif self.oper == '/' and self.y != '0':
-            print(float(self.x) / float(self.y))
-            return False
-        else:
-            print(self.msg_3)
+
+        self.x = float(self.x)
+        self.y = float(self.y)
+
+        if self.operator in ["+", "-", "*", "/"]:
+            if self.operator == "/" and self.y == 0:
+                print(Hcalc.msg_3)
+                return True
+            else:
+                print(Hcalc.operations[self.operator](self.x, self.y))
+
+        if Hcalc.store_continue(self) is True:
             return True
 
 
 z = True
 while z is True:
-    x_, oper_, y_ = input('Enter an equation').split()
-    operation = Operation(x=x_, oper=oper_, y=y_)
-    z = operation.control_func()
+    x_, operator_, y_ = input('Enter an equation').split()
+    calc = Hcalc(x=x_, operator=operator_, y=y_)
+    z = calc.control_func()
